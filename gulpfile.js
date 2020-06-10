6b4159a5fb901e8a7324b90d117b089104877385
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2020-06-10 22:52:07 +0530

    added copy html gulp-task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-const gulp =require('gulp');
+const gulp = require('gulp');
 
-gulp.task('message',async ()=>{
-     console.log("Gulp is running....");
-        });
+
+// Log message
+gulp.task('message', async () => {
+    console.log("Gulp is running....");
+});
+
+// Copy html file
+gulp.task('copyHtml', async () => {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'))
+});
